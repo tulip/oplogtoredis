7ae922891857f43aa6a8b5d382f66f840f0d658f
Nathan Perry <np@nathanperry.dev>
2019-08-30 12:34:30 -0400

    turn off unsupported mongod flag in versions >= 4

--- scripts/runIntegrationAcceptance.sh
+++ scripts/runIntegrationAcceptance.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -eu
-cd `dirname "$0"`'/../integration-tests/acceptance'
+cd "$(dirname "$0")/../integration-tests/acceptance"
 
 # Use docker-compose to spin up the test environment
 mongo_tags=( "3.6.11" "3.4.14" "3.2.19" "4.0.12" "4.2.0" )
@@ -24,6 +24,15 @@ for mongo_tag in "${mongo_tags[@]}"; do
             export MONGO_TAG="$mongo_tag"
             export REDIS_TAG="$redis_tag"
             export OTR_DOCKERFILE="$otr_dockerfile"
+
+            MONGO_ARGS=""
+
+            read -r -d . MAJOR <<<"$MONGO_TAG"
+            if [[ $MAJOR -lt 4 ]]; then
+                MONGO_ARGS="--smallFiles"
+            fi
+
+            export MONGO_ARGS
 
             docker-compose rm -vf
             docker-compose down -v
