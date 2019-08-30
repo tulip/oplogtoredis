a7d3f70c449c438b59a5acd1629b483401a6dbda
Nathan Perry <np@nathanperry.dev>
2019-08-30 11:17:41 -0400

    add mongo 4 to acceptance

--- scripts/runIntegrationAcceptance.sh
+++ scripts/runIntegrationAcceptance.sh
@@ -4,7 +4,7 @@ set -eu
 cd `dirname "$0"`'/../integration-tests/acceptance'
 
 # Use docker-compose to spin up the test environment
-mongo_tags=( "3.6.11" "3.4.14" "3.2.19" )
+mongo_tags=( "3.6.11" "3.4.14" "3.2.19" "4.0.12" "4.2.0" )
 redis_tags=( "3.2.4" "4.0.9" )
 otr_dockerfiles=( "Dockerfile" "Dockerfile.racedetector" )
 
