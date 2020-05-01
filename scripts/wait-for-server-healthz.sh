4c471b835e282f5e6830a2c835185bc13afee3a3
Zhiyan Foo <zhiyanfoo@gmail.com>
2020-05-01 17:50:44 -0400

    fix spelling and indentation

--- scripts/wait-for-server-healthz.sh
+++ scripts/wait-for-server-healthz.sh
@@ -4,12 +4,12 @@ ATTEMPT_COUNTER=0
 MAX_ATTEMPTS=15
 echo "Waiting for server to come up"
 until $(curl --output /dev/null --silent --head --fail localhost:9000/healthz); do
-	if [ ${ATTEMPT_COUNTER} -eq ${MAX_ATTEMPTS} ];then
-		echo "Max attempts reached"
-		exit 1
-	fi
+  if [ ${ATTEMPT_COUNTER} -eq ${MAX_ATTEMPTS} ];then
+    echo "Max attempts reached"
+    exit 3
+  fi
   printf '.'
   sleep 1
   ATTEMPT_COUNTER=$(($ATTEMPT_COUNTER+1))
 done
-echo "Succesfully hit healthz endpoint"
+echo "Successfully hit healthz endpoint"
