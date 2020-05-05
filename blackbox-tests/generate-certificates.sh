395cb8ec794d50fa5f7ae4c6a46a59aa9a79e913
Zhiyan Foo <zhiyanfoo@gmail.com>
2020-05-05 17:35:51 -0400

    Change CA cert details

--- blackbox-tests/generate-certificates.sh
+++ blackbox-tests/generate-certificates.sh
@@ -3,7 +3,7 @@ set -eu
 
 openssl genrsa -out ca-key.pem 4096
 openssl req -new -x509 -days 365 -key ca-key.pem -sha256 -out ca.pem \
-  -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=example.com"
+  -subj "/C=US/ST=MA/L=Boston/O=Tulip QA/OU=techops/CN=example.com"
 openssl genrsa -out server-key.pem 4096
 openssl req -subj "/CN=localhost" -sha256 -new -key server-key.pem -out server.csr
 echo "subjectAltName = DNS:redis_tls" > extfile.cnf
