c5f5a00786917030cd3e1593bde64ff7f2103e59
Alex Goodisman <alex.goodisman@tulip.co>
2024-04-04 19:08:53 -0400

    add actual oplog deny test

--- integration-tests/acceptance/denylist_oplog_test.go
+++ integration-tests/acceptance/denylist_oplog_test.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
 	"testing"
+
+	"github.com/tulip/oplogtoredis/integration-tests/helpers"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func TestDenyOplog(t *testing.T) {
@@ -20,4 +24,70 @@ func TestDenyOplog(t *testing.T) {
 
 	// send message
 	// confirm message arrived
+
+	harness := startHarness()
+	defer harness.stop()
+
+	_, err := harness.mongoClient.Collection("Foo").InsertOne(context.Background(), bson.M{
+		"_id": "id1",
+		"f":   "1",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	expectedMessage1 := helpers.OTRMessage{
+		Event: "i",
+		Document: map[string]interface{}{
+			"_id": "id1",
+		},
+		Fields: []string{"_id", "f"},
+	}
+
+	harness.verify(t, map[string][]helpers.OTRMessage{
+		"tests.Foo":      {expectedMessage1},
+		"tests.Foo::id1": {expectedMessage1},
+	})
+
+	ruleID := doRequest("PUT", "/denylist", map[string]interface{}{
+		"keys":  "ns",
+		"regex": "^tests.Foo$",
+	}, t, 201).(string)
+
+	_, err = harness.mongoClient.Collection("Foo").InsertOne(context.Background(), bson.M{
+		"_id": "id2",
+		"g":   "2",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	harness.verify(t, map[string][]helpers.OTRMessage{
+		"tests.Foo":      {},
+		"tests.Foo::id2": {},
+	})
+
+	doRequest("DELETE", "/denylist/"+ruleID, map[string]interface{}{}, t, 204)
+
+	_, err = harness.mongoClient.Collection("Foo").InsertOne(context.Background(), bson.M{
+		"_id": "id3",
+		"h":   "3",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	expectedMessage3 := helpers.OTRMessage{
+		Event: "i",
+		Document: map[string]interface{}{
+			"_id": "id3",
+		},
+		Fields: []string{"_id", "h"},
+	}
+
+	harness.verify(t, map[string][]helpers.OTRMessage{
+		"tests.Foo":      {expectedMessage3},
+		"tests.Foo::id3": {expectedMessage3},
+	})
+
 }
