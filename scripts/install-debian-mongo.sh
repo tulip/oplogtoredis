39a07cf7d313b1736aa6fb686be54411519d796f
Clark Jacobsohn <clark@tulip.co>
2025-07-28 11:34:26 -0400

    Upgrade debian tags for dev and test images

--- scripts/install-debian-mongo.sh
+++ scripts/install-debian-mongo.sh
@@ -3,7 +3,7 @@ apt-get install -y wget gnupg
 
 wget -qO - https://pgp.mongodb.com/server-5.0.asc | gpg -o /usr/share/keyrings/mongodb-server-5.0.gpg --dearmor
 
-echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-5.0.gpg ] https://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" > /etc/apt/sources.list.d/mongodb-org-5.0.list
+echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-5.0.gpg ] https://repo.mongodb.org/apt/debian bullseye/mongodb-org/5.0 main" > /etc/apt/sources.list.d/mongodb-org-5.0.list
 
 apt-get update
 apt-get install -y mongodb-org-server=5.0.19 mongodb-org-shell=5.0.19
